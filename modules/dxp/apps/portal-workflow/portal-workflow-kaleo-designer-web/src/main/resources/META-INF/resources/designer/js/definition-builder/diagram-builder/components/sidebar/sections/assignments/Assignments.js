f1bb4faf2aa2a98a6fb4d597af45f20ac553a3c9
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-01-28 19:06:44 -0300

    wip Display current assignment

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/assignments/Assignments.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/assignments/Assignments.js
@@ -9,12 +9,14 @@
  * distribution rights of the Software.
  */
 
-import ClayButton from '@clayui/button';
+import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
+import ClayLayout from '@clayui/layout';
 import PropTypes from 'prop-types';
 import React, {useContext} from 'react';
 
 import {DiagramBuilderContext} from '../../../../DiagramBuilderContext';
 import SidebarPanel from '../../SidebarPanel';
+import {options} from './SelectAssignment';
 
 const Assignments = ({setContentName}) => {
 	const {selectedItem} = useContext(DiagramBuilderContext);
@@ -31,15 +33,52 @@ const Assignments = ({setContentName}) => {
 				</ClayButton>
 			) : (
 				<CurrentAssignments
-					assignments={selectedItem.data.assignments}
+					assignments={{...selectedItem.data.assignments}}
 				/>
 			)}
 		</SidebarPanel>
 	);
 };
 
-const CurrentAssignments = () => {
-	return <p>Current Assignments:</p>;
+const CurrentAssignments = ({assignments}) => {
+	const {setSelectedItem} = useContext(DiagramBuilderContext);
+	const deleteCurrentAssignments = () => {
+		setSelectedItem((previousValue) => ({
+			...previousValue,
+			data: {
+				...previousValue.data,
+				assignments: null,
+			},
+		}));
+	};
+
+	return (
+		<ClayLayout.ContentCol className="current-assignments-area" float>
+			<ClayLayout.Row
+				className="current-assignments-row"
+				justify="between"
+			>
+				<span>
+					{
+						options.find(
+							(option) =>
+								option.assignmentType ===
+								assignments.assignmentType[0]
+						)?.label
+					}
+
+					{/* {Liferay.Language.get(assignments.assignmentType[0])} */}
+				</span>
+
+				<ClayButtonWithIcon
+					className="delete-button text-secondary trash-button"
+					displayType="unstyled"
+					onClick={deleteCurrentAssignments}
+					symbol="trash"
+				/>
+			</ClayLayout.Row>
+		</ClayLayout.ContentCol>
+	);
 };
 
 Assignments.propTypes = {
