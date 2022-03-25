4787c0f48d806e0f3f54dca8c7041e490f9c8354
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-03-25 01:57:00 -0300

    wip

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/TimerAction.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/timers/TimerAction.js
@@ -11,7 +11,7 @@
 
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React from 'react';
 
 import SidebarPanel from '../../SidebarPanel';
 import ActionTypeAction from './select-action/ActionTypeAction';
@@ -20,21 +20,22 @@ import ActionTypeReassignment from './select-action/ActionTypeReassignment';
 import SelectActionType from './select-action/SelectActionType';
 
 const actionSectionComponents = {
-	actions: ActionTypeAction,
-	notifications: ActionTypeNotification,
 	reassignments: ActionTypeReassignment,
+	timerActions: ActionTypeAction,
+	timerNotifications: ActionTypeNotification,
 };
 
 const TimerAction = ({
+	actionData,
 	actionSectionsIndex,
-	identifier,
+	reassignments,
 	sectionsLength,
 	setActionSections,
+	setErrors,
 	timersIndex,
 }) => {
-	const [actionType, setActionType] = useState('actions');
-
-	const ActionSectionComponent = actionSectionComponents[actionType];
+	const ActionSectionComponent =
+		actionSectionComponents[actionData.actionType];
 
 	const deleteSection = (identifier) => {
 		setActionSections((prevSections) => {
@@ -49,6 +50,7 @@ const TimerAction = ({
 	const handleClickNew = (prev) => [
 		...prev,
 		{
+			actionType: 'timerActions',
 			identifier: `${Date.now()}-${prev.length}`,
 		},
 	];
@@ -56,20 +58,24 @@ const TimerAction = ({
 	return (
 		<SidebarPanel panelTitle={Liferay.Language.get('action')}>
 			<SelectActionType
-				actionType={actionType}
-				setActionType={setActionType}
-			/>
-
-			<ActionSectionComponent
 				actionSectionsIndex={actionSectionsIndex}
-				actionType={actionType}
-				identifier={identifier}
-				key={`section-${identifier}`}
-				sectionsLength={sectionsLength}
+				actionType={actionData.actionType}
+				reassignments={reassignments}
 				setActionSections={setActionSections}
 				timersIndex={timersIndex}
 			/>
-			
+
+			<ActionSectionComponent
+				actionData={actionData}
+				actionSectionsIndex={actionSectionsIndex}
+				actionType={actionData.actionType}
+				key={`section-${actionData.identifier}`}
+				sectionsLength={sectionsLength}
+				setActionSections={setActionSections}
+				setErrors={setErrors}
+				timersIndex={timersIndex}
+			/>
+
 			<div className="autofit-float autofit-padded-no-gutters-x autofit-row autofit-row-center mb-3">
 				<div className="autofit-col">
 					<ClayButton
@@ -88,7 +94,7 @@ const TimerAction = ({
 						<ClayButtonWithIcon
 							className="delete-button"
 							displayType="unstyled"
-							onClick={() => deleteSection(identifier)}
+							onClick={() => deleteSection(actionData.identifier)}
 							symbol="trash"
 						/>
 					)}
