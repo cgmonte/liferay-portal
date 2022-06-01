1b2a497650144d360faf9119f2e9f1d6699ba4cd
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-06-01 15:26:33 -0300

    LPS-154881 Add feature flag

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/frontend/taglib/servlet/taglib/ObjectDefinitionsStatesScreenNavigationCategory.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/frontend/taglib/servlet/taglib/ObjectDefinitionsStatesScreenNavigationCategory.java
@@ -21,7 +21,10 @@ import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.web.internal.object.definitions.constants.ObjectDefinitionsScreenNavigationEntryConstants;
 import com.liferay.object.web.internal.object.definitions.display.context.ObjectDefinitionsStatesDisplayContext;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
@@ -72,6 +75,10 @@ public class ObjectDefinitionsStatesScreenNavigationCategory
 
 	@Override
 	public boolean isVisible(User user, ObjectDefinition objectDefinition) {
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-152677"))) {
+			return false;
+		}
+
 		return !objectDefinition.isSystem();
 	}
 
