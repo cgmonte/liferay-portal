04fe643dfd6639c197c48022728e54abfda66ad1
Rafael Praxedes <rafael.praxedes@liferay.com>
2022-10-31 12:29:07 -0300

    LPS-167028 Provide client extension informations to the frontend code

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/KaleoDesignerPortlet.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/java/com/liferay/portal/workflow/kaleo/designer/web/internal/portlet/KaleoDesignerPortlet.java
@@ -55,6 +55,7 @@ import com.liferay.portal.kernel.util.comparator.UserFirstNameComparator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.workflow.configuration.WorkflowDefinitionConfiguration;
 import com.liferay.portal.workflow.kaleo.designer.web.constants.KaleoDesignerPortletKeys;
+import com.liferay.portal.workflow.kaleo.designer.web.internal.client.extension.ClientExtensionTracker;
 import com.liferay.portal.workflow.kaleo.designer.web.internal.constants.KaleoDesignerWebKeys;
 import com.liferay.portal.workflow.kaleo.designer.web.internal.portlet.display.context.KaleoDesignerDisplayContext;
 import com.liferay.portal.workflow.kaleo.exception.DuplicateKaleoDefinitionNameException;
@@ -490,7 +491,8 @@ public class KaleoDesignerPortlet extends MVCPortlet {
 
 		KaleoDesignerDisplayContext kaleoDesignerDisplayContext =
 			new KaleoDesignerDisplayContext(
-				renderRequest, _kaleoDefinitionVersionLocalService,
+				_clientExtensionTracker, renderRequest,
+				_kaleoDefinitionVersionLocalService,
 				ResourceBundleLoaderUtil.getPortalResourceBundleLoader(),
 				_userLocalService);
 
@@ -534,6 +536,9 @@ public class KaleoDesignerPortlet extends MVCPortlet {
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private ClientExtensionTracker _clientExtensionTracker;
 
 	private volatile boolean _companyAdministratorCanPublish;
 
