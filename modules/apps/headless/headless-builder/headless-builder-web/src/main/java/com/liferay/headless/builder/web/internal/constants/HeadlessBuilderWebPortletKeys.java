73cfcbe3a1e6442098363d69a7eebb55399183c9
Carlos Montenegro <carlos.montenegro@liferay.com>
2023-05-18 18:48:13 -0300

    wip blade e ajustes

---
+++ modules/apps/headless/headless-builder/headless-builder-web/src/main/java/com/liferay/headless/builder/web/internal/constants/HeadlessBuilderWebPortletKeys.java
@@ -0,0 +1,10 @@
+package com.liferay.headless.builder.web.internal.constants;
+
+/**
+ * @author Carlos Montenegro
+ */
+public class HeadlessBuilderWebPortletKeys {
+
+	public static final String HEADLESSBUILDERWEB =
+		"com_liferay_headless_builder_web_internal_portlet_HeadlessBuilderWebPortlet";
+}
