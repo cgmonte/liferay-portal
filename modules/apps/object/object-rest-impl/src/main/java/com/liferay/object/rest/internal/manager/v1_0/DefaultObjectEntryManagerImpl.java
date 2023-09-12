0ccb94a0706b3fc3cd74499cb3328305f6cc5770
Alejandro Tardín <alejandro.tardin@liferay.com>
2023-09-12 13:29:43 +0200

    LPS-195526 Using only service builder models
    We were mixing service builder models with ObjectEntries from the REST
    layer for no real reason.
    
    Now we are using only service builder models both for custom and system
    object entries.
    
    This means all of them have the same interface and the external reference
    code can be retrieved in the same way.

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -240,9 +240,9 @@ public class DefaultObjectEntryManagerImpl
 
 		long[] relatedPrimaryKeys = TransformUtil.transformToLongArray(
 			_getRelatedModels(
-				dtoConverterContext, objectDefinition, objectRelationship,
-				primaryKey, relatedObjectDefinition),
-			this::_getPrimaryKey);
+				objectDefinition, objectRelationship, primaryKey,
+				relatedObjectDefinition),
+			BaseModel::getPrimaryKeyObj);
 
 		_disassociateRelatedModels(
 			objectDefinition, objectRelationship, primaryKey,
@@ -952,17 +952,20 @@ public class DefaultObjectEntryManagerImpl
 			long[] toDisassociatePrimaryKeys =
 				TransformUtil.transformToLongArray(
 					_getRelatedModels(
-						dtoConverterContext, objectDefinition,
-						objectRelationship, primaryKey,
+						objectDefinition, objectRelationship, primaryKey,
 						relatedObjectDefinition),
 					relatedModel -> {
+						ExternalReferenceCodeModel externalReferenceCodeModel =
+							(ExternalReferenceCodeModel)relatedModel;
+
 						if (nestedExternalReferenceCodes.contains(
-								_getExternalReferenceCode(relatedModel))) {
+								externalReferenceCodeModel.
+									getExternalReferenceCode())) {
 
 							return null;
 						}
 
-						return _getPrimaryKey(relatedModel);
+						return relatedModel.getPrimaryKeyObj();
 					});
 
 			if (toDisassociatePrimaryKeys.length > 0) {
@@ -1130,25 +1133,7 @@ public class DefaultObjectEntryManagerImpl
 		return QueryUtil.ALL_POS;
 	}
 
-	private String _getExternalReferenceCode(Object relatedModel) {
-		if (relatedModel instanceof ExternalReferenceCodeModel) {
-			ExternalReferenceCodeModel externalReferenceCodeModel =
-				(ExternalReferenceCodeModel)relatedModel;
-
-			return externalReferenceCodeModel.getExternalReferenceCode();
-		}
-		else if (relatedModel instanceof ObjectEntry) {
-			ObjectEntry objectEntry = (ObjectEntry)relatedModel;
-
-			return objectEntry.getExternalReferenceCode();
-		}
-
-		return null;
-	}
-
-	private Object _getManyToOneRelatedModel(
-			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition,
+	private BaseModel<ExternalReferenceCodeModel> _getManyToOneRelatedModel(
 			ObjectRelationship objectRelationship, long primaryKey,
 			ObjectDefinition relatedObjectDefinition)
 		throws Exception {
@@ -1168,9 +1153,17 @@ public class DefaultObjectEntryManagerImpl
 				objectRelationship.getObjectRelationshipId(), primaryKey);
 		}
 
-		return fetchRelatedManyToOneObjectEntry(
-			dtoConverterContext, objectDefinition, primaryKey,
-			objectRelationship.getName());
+		ManyToOneObjectRelatedModelsProvider objectRelatedModelsProvider =
+			(ManyToOneObjectRelatedModelsProvider)
+				_objectRelatedModelsProviderRegistry.
+					getObjectRelatedModelsProvider(
+						relatedObjectDefinition.getClassName(),
+						relatedObjectDefinition.getCompanyId(),
+						objectRelationship.getType());
+
+		return objectRelatedModelsProvider.fetchRelatedModel(
+			GroupThreadLocal.getGroupId(),
+			objectRelationship.getObjectRelationshipId(), primaryKey);
 	}
 
 	private String _getObjectEntriesPermissionName(long objectDefinitionId) {
@@ -1220,23 +1213,7 @@ public class DefaultObjectEntryManagerImpl
 		return objectRelationships;
 	}
 
-	private long _getPrimaryKey(Object relatedModel) {
-		if (relatedModel instanceof BaseModel<?>) {
-			BaseModel<?> baseModel = (BaseModel<?>)relatedModel;
-
-			return (long)baseModel.getPrimaryKeyObj();
-		}
-		else if (relatedModel instanceof ObjectEntry) {
-			ObjectEntry objectEntry = (ObjectEntry)relatedModel;
-
-			return objectEntry.getId();
-		}
-
-		return 0;
-	}
-
-	private List<Object> _getRelatedModels(
-			DTOConverterContext dtoConverterContext,
+	private List<? extends BaseModel<?>> _getRelatedModels(
 			ObjectDefinition objectDefinition,
 			ObjectRelationship objectRelationship, long primaryKey,
 			ObjectDefinition relatedObjectDefinition)
@@ -1248,8 +1225,7 @@ public class DefaultObjectEntryManagerImpl
 
 			return Collections.singletonList(
 				_getManyToOneRelatedModel(
-					dtoConverterContext, objectDefinition, objectRelationship,
-					primaryKey, relatedObjectDefinition));
+					objectRelationship, primaryKey, relatedObjectDefinition));
 		}
 
 		ObjectRelatedModelsProvider<?> objectRelatedModelsProvider =
@@ -1270,7 +1246,7 @@ public class DefaultObjectEntryManagerImpl
 					objectRelationship.getName());
 		}
 
-		return (List<Object>)objectRelatedModelsProvider.getRelatedModels(
+		return objectRelatedModelsProvider.getRelatedModels(
 			GroupThreadLocal.getGroupId(),
 			objectRelationship.getObjectRelationshipId(), primaryKey, null, -1,
 			-1);
