c2090ea871756e4ad8c34d11da0a8e15247bf0dc
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-12-01 10:29:20 -0300

    LPS-202448 Create Page Object Model

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/LeftSidebar/LeftSidebar.tsx
@@ -141,6 +141,7 @@ export default function LeftSidebar() {
 							!!leftSidebarItems.length && (
 								<>
 									<LeftSidebarTreeView
+
 										expandedKeys={expandedKeys}
 										leftSidebarOtherObjectFoldersItems={
 											leftSidebarOtherObjectFoldersItems
