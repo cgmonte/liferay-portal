6fe828aa248b08ec70a870828200095aea61e54a
MarinhoFeliphe <feliphemarinho18@gmail.com>
2023-12-05 17:26:25 -0300

    LPS-202448 Not need

--- modules/test/playwright/fixtures/homePage.fixture.js
+++ modules/test/playwright/fixtures/homePage.fixture.js
@@ -3,27 +3,12 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {expect, test} from '@playwright/test';
+import {test} from '@playwright/test';
 
-import {liferayConfig} from '../liferay.config';
 import {HomePage} from '../pages/home.page';
-
-const _AUTH_FILE = 'tmp/.auth/user.json';
 
 exports.test = test.extend({
 	_homePage: async ({page}, use) => {
 		await use(new HomePage(page));
-	},
-	_signedInHomePage: async ({page}, use) => {
-		const homePage = new HomePage(page);
-		await homePage.login(
-			liferayConfig.user.login,
-			liferayConfig.user.password
-		);
-		await expect(
-			page.getByLabel('Open Applications MenuCtrl+')
-		).toBeVisible();
-		await page.context().storageState({path: _AUTH_FILE});
-		await use(homePage);
 	},
 });
