169106f16f153076dbc228a059bc5f74a5789410
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-04-08 09:21:47 -0300

    LPP-53369 When destroying its instance, CKEditor calls setHtml, which looks for HTML in its content and parses it, attaching whatever event listeners it finds. This is unsafe, so here we validate the content just before the editor's instance is destroyed and, if something invalid is found, we set the content to an empty string, with nothing to be parsed and executed. The valid source builder content is handled and persisted way before the destroying, so nothing is lost. With this solution, looking for alert() calls is not necessary, so we are also deleting the related logic here.

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/source-builder/SourceBuilder.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/source-builder/SourceBuilder.js
@@ -13,12 +13,11 @@ import {ClassicEditor} from 'frontend-editor-ckeditor-web';
 import React, {useContext, useEffect, useRef, useState} from 'react';
 import {isEdge, isNode} from 'react-flow-renderer';
 
+import XMLUtil from '../../../js/definition-builder/source-builder/xmlUtil';
 import {DefinitionBuilderContext} from '../DefinitionBuilderContext';
 import {editorConfig} from '../constants';
 import {xmlNamespace} from './constants';
 import {serializeDefinition} from './serializeUtil';
-
-const REGEX_ALERT = /alert\((.*?)\)/;
 
 export default function SourceBuilder() {
 	const {
@@ -54,22 +53,6 @@ export default function SourceBuilder() {
 				);
 
 				if (xmlContent) {
-					const codeEditor = document.querySelector(
-						'div.cke_contents'
-					);
-
-					codeEditor.addEventListener('keyup', () => {
-						if (currentEditor.getData() !== xmlContent) {
-							const newXmlContent = currentEditor.getData();
-							const sanitizedXmlContent = newXmlContent.replace(
-								REGEX_ALERT,
-								''
-							);
-
-							currentEditor.setData(sanitizedXmlContent);
-						}
-					});
-
 					currentEditor.setData(xmlContent);
 
 					setLoading(false);
@@ -126,13 +109,6 @@ export default function SourceBuilder() {
 
 			reader.onloadend = (event) => {
 				if (event.target.readyState === FileReader.DONE) {
-					const sanitizedData = event.target.result.replace(
-						REGEX_ALERT,
-						''
-					);
-
-					currentEditor.setData(sanitizedData);
-
 					const fileInput = document.querySelector('#fileInput');
 
 					fileInput.value = '';
@@ -196,6 +172,14 @@ export default function SourceBuilder() {
 			<ClassicEditor
 				config={editorConfig}
 				name="sourceBuilderEditor"
+				onBeforeDestroy={({editor}) => {
+					if (
+						editor.checkDirty() &&
+						!XMLUtil.validateDefinition(editor.getData())
+					) {
+						editor.setData('');
+					}
+				}}
 				onInstanceReady={({editor}) => {
 					editor.setMode('source');
 
