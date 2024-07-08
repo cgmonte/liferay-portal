bd1d600e3f2df61128497a6e5be1b5a5d8736e75
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-07-08 22:21:57 +0200

    ble

--- modules/apps/generative-ai/generative-ai-web/src/main/resources/META-INF/resources/task_definitions_admin/js/utils/tasks-flow/getElementsFromTaskConfig.js
+++ modules/apps/generative-ai/generative-ai-web/src/main/resources/META-INF/resources/task_definitions_admin/js/utils/tasks-flow/getElementsFromTaskConfig.js
@@ -3,129 +3,50 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import { NAME_LABELS } from "../constants";
-
-// function getNodeLabel(taskName) {
-//     if (taskName.includes('agent')) {
-//         return `Agent: ${NAME_LABELS[taskName]}`;
-//     }
-
-//     return `Task: ${NAME_LABELS[taskName]}`;
-// }
-
-// let previousPosition;
-
-// function getElementPosition(previous, name) {
-//     if (previous === undefined) {
-//         previousPosition = { x: 0, y: 0 };
-//         return previousPosition;
-//     }
-
-//     previousPosition = { x: previous.x, y: previous.y + 150 };
-
-//     return previousPosition;
-// }
-
-// function getElement(previousPosition, taskConfig) {
-//     const element = {};
-
-//     element.id = taskConfig.id;
-//     element.className = 'light';
-
-//     if (element.id === 1) {
-//         element.type = 'input';
-//         const position = { x: 0, y: 0 };
-
-//         element.position = position;
-//         previousPosition = position;
-//     } else {
-//         element.type = 'default';
-//         const position = getElementPosition(previousPosition, taskConfig.name);
-
-//         element.position = position;
-//         previousPosition = position;
-//     }
-//     const data = {};
-
-//     data.label = getNodeLabel(taskConfig.name);
-//     data.taskConfig = taskConfig;
-
-//     element.data = data;
-
-//     return element;
-// }
-
 export default function getElementsFromTaskConfig(taskConfig) {
-    let idCounter = 1;
     let elements = [];
 
-    function traverseObject(obj, depth = 0, parentId = null) {
-        let currentId = null;
+    function traverseObject(obj) {
         for (let key in obj) {
-          if (typeof obj[key] === 'object' && obj[key] !== null) {
-            currentId = String(idCounter++);
-            let node = {
-              id: currentId,
-              data: { label: key },
-              position: { x: 100, y: depth * 100 },
-            };
-      
-            if (key === 'name') {
-              node.data.label = NAME_LABELS[obj[key]];
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                traverseObject(obj[key]);
+            } else {
+                console.log(key, obj[key]);
             }
-      
-            if (key === 'attributes') {
-              node.data.attributes = obj[key];
-            }
-      
-            elements.push(node);
-      
-            if (parentId) {
-              elements.push({
-                id: `e${parentId}-${currentId}`,
-                source: parentId,
-                target: currentId,
-                animated: true,
-              });
-            }
-      
-            traverseObject(obj[key], depth + 1, currentId);
-          }
         }
-      }
-      
-    //   // Usage
-    //   const schema = {
-    //     "properties": {
-    //       "attributes": {
-    //         "$ref": "#/definitions/taskAttributes"
-    //       },
-    //       "debug": {
-    //         "default": false,
-    //         "description": "Include debug information in the response",
-    //         "type": "boolean"
-    //       },
-    //       "name": {
-    //         "default": "gemini_chat_model",
-    //         "description": "The name of the task",
-    //         "enum": [
-    //           "chain",
-    //           "gemini_chat_model",
-    //           "google_imagen",
-    //           "local_document_retrieval",
-    //           "openai_image_model",
-    //           "task_context_agent",
-    //           "text_input_agent",
-    //           "webhook"
-    //         ],
-    //         "type": "string"
-    //       }
-    //     },
-    //     "type": "object"
-    //   };
-      
-      traverseObject(taskConfig);
-      console.log(elements);
+    }
 
-    return elements
+    if (taskConfig && Object.keys(taskConfig).length) {
+        traverseObject(taskConfig);
+
+        consoleg.log(elements);
+
+        // return elements;
+    }
+
+    return null;
 };
+
+// if tis chain, always look for attributes.tasks
+
+const processedElements = [
+    { id: '1', type: 'input', data: { label: 'Chain' }, position: { x: 0, y: 0 }, className: 'light' },
+    { id: 'e1-2', source: '1', target: '2'},
+    { id: '2', type: 'input', data: { label: 'Chain' }, position: { x: 0, y: 0 }, className: 'light' },
+
+
+
+
+
+
+
+
+
+    
+    { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 }, className: 'light' },
+    { id: '3', data: { label: 'Node 3' }, position: { x: 400, y: 100 }, className: 'light' },
+    { id: '4', data: { label: 'Node 4' }, position: { x: 400, y: 200 }, className: 'light' },
+    { id: 'e1-2', source: '1', target: '2', animated: true },
+    { id: 'e1-3', source: '1', target: '3' },
+  ];
+  
