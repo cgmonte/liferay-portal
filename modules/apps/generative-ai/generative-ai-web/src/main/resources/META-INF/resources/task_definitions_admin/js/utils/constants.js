6df81ebf8662396cb89dea0ecbe74b5ede30be6a
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-07-10 11:46:14 +0200

    Add constants

--- modules/apps/generative-ai/generative-ai-web/src/main/resources/META-INF/resources/task_definitions_admin/js/utils/constants.js
+++ modules/apps/generative-ai/generative-ai-web/src/main/resources/META-INF/resources/task_definitions_admin/js/utils/constants.js
@@ -25,3 +25,44 @@ export const DEFAULT_INDEX_CONFIGURATION = {
 export const SXP_ELEMENT_PREFIX = {
 	QUERY: 'querySXPElement',
 };
+
+export const NAME_LABELS = {
+	chain: "Chain",
+	gemini_chat_model: "Gemini Chat Model",
+	google_imagen: "Google Imagen",
+	local_document_retrieval: "Local Document Retrieval",
+	retrieve_local_documents: "Retrieve Local Documents",
+	openai_image_model: "OpenAI Image Model",
+	task_context_agent: "Task Context Agent",
+	task_context_parameter_agent: "Task Context Parameter Agent",
+	text_input_agent: "Text Input Agent",
+	webhook: "Webhook",
+}
+
+export const ATTRIBUTES_LABELS = {
+	context_output_parameter_name: "Context Output Parameter Name",
+	location: "Location",
+	max_output_tokens: "Max Output Tokens",
+	memory_max_messages: "Memory Max Messages",
+	model_name: "Model Name",
+	project: "Project",
+	prompt_template: "Prompt Template",
+	system_message: "System Message",
+	top_k: "Top K",
+	webhook: "Webhook",
+	use_chat_memory: "Use Chat Memory",
+}
+
+export const ATTRIBUTES_INPUT_TYPES = {
+	context_output_parameter_name: "text",
+	location: "text",
+	max_output_tokens: "number",
+	memory_max_messages: "number",
+	model_name: "text",
+	project: "text",
+	prompt_template: "text",
+	system_message: "text",
+	top_k: "number",
+	webhook: "text",
+	use_chat_memory: "checkbox",
+}
