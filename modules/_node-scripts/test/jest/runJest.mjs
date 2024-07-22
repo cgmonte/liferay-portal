8cc8c1bdda724b69bb590e734559cc03a8c7ca4d
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-07-22 09:27:11 -0700

    Updates

--- modules/_node-scripts/test/jest/runJest.mjs
+++ modules/_node-scripts/test/jest/runJest.mjs
@@ -62,7 +62,7 @@ export default async function runJest({
 
 		const childProcess = $(
 			config
-		)`jest --projects ${projectDir} --config ${CONFIG_PATH} --silent ${cliFlags.join(' ')}`;
+		)`jest ${projectDir} --config ${CONFIG_PATH} --silent ${cliFlags.join(' ')}`;
 
 		result = await childProcess;
 	}
