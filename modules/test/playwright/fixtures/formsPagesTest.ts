b480a93679af51ff86ecaa5ece365afeadb4be22
Carlos Montenegro <carlos.montenegro@liferay.com>
2024-11-19 12:19:43 -0300

    LPD-41453 Create Playwright pages for data provider, forms field settings sidebar and rules builder

--- modules/test/playwright/fixtures/formsPagesTest.ts
+++ modules/test/playwright/fixtures/formsPagesTest.ts
@@ -5,25 +5,37 @@
 
 import {test} from '@playwright/test';
 
+import {DataProviderPage} from '../pages/dynamic-data-mapping-form-web/DataProviderPage';
+import {FormBuilderFieldSettingsSidePanelPage} from '../pages/dynamic-data-mapping-form-web/FormBuilderFieldSettingsSidePanelPage';
 import {FormBuilderPage} from '../pages/dynamic-data-mapping-form-web/FormBuilderPage';
 import {FormBuilderSidePanelPage} from '../pages/dynamic-data-mapping-form-web/FormBuilderSidePanelPage';
 import {FormSettingsModalPage} from '../pages/dynamic-data-mapping-form-web/FormSettingsModalPage';
 import {FormWidgetPage} from '../pages/dynamic-data-mapping-form-web/FormWidgetPage';
 import {FormsPage} from '../pages/dynamic-data-mapping-form-web/FormsPage';
+import {RulesBuilderPage} from '../pages/dynamic-data-mapping-form-web/RulesBuilderPage';
 import {PageEditorPage} from '../pages/layout-content-page-editor-web/PageEditorPage';
 import {ConfigurationTabPage} from '../pages/portal-workflow-kaleo-designer-web/ConfigurationTabPage';
 
 const formsPagesTest = test.extend<{
 	configurationTabPage: ConfigurationTabPage;
+	dataProviderPage: DataProviderPage;
+	formBuilderFieldSettingsSidePanelPage: FormBuilderFieldSettingsSidePanelPage;
 	formBuilderPage: FormBuilderPage;
 	formBuilderSidePanelPage: FormBuilderSidePanelPage;
 	formSettingsModalPage: FormSettingsModalPage;
 	formWidgetPage: FormWidgetPage;
 	formsPage: FormsPage;
 	pageEditorPage: PageEditorPage;
+	rulesBuilderPage: RulesBuilderPage;
 }>({
 	configurationTabPage: async ({page}, use) => {
 		await use(new ConfigurationTabPage(page));
+	},
+	dataProviderPage: async ({page}, use) => {
+		await use(new DataProviderPage(page));
+	},
+	formBuilderFieldSettingsSidePanelPage: async ({page}, use) => {
+		await use(new FormBuilderFieldSettingsSidePanelPage(page));
 	},
 	formBuilderPage: async ({page}, use) => {
 		await use(new FormBuilderPage(page));
@@ -42,6 +54,9 @@ const formsPagesTest = test.extend<{
 	},
 	pageEditorPage: async ({page}, use) => {
 		await use(new PageEditorPage(page));
+	},
+	rulesBuilderPage: async ({page}, use) => {
+		await use(new RulesBuilderPage(page));
 	},
 });
 
