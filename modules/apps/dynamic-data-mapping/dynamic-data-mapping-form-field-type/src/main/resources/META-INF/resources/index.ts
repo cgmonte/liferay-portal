f9e7ddd18843c4402a82b11fcdc32c3b664c1299
Carlos Montenegro <carlos.montenegro@liferay.com>
2025-02-10 18:49:49 -0300

    LPD-48033 Create MultipleSelectLocalizedObjectField component

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/index.ts
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/index.ts
@@ -102,7 +102,7 @@ export {default as Numeric} from './Numeric/Numeric';
 export {default as NumericInputMask} from './NumericInputMask/NumericInputMask';
 export {default as MultipleSelection} from './Select/MultipleSelect';
 export {default as Select} from './Select/Select';
-export type {FieldChangeEventHandler} from './types';
+export type {FieldChangeEventHandler, LocalizedValue} from './types';
 
 export {
 	Captcha,
