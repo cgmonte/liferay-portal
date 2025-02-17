b528fc13143c244ee0f96776f35605513eea217e
Carlos Montenegro <carlos.montenegro@liferay.com>
2025-02-17 14:50:37 -0300

    LPD-49119 Display Translation Options section in the object's field-editing sidepanel for MultiselectPicklist business type

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/BasicInfo/TranslationOptionsContainer.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Tabs/BasicInfo/TranslationOptionsContainer.tsx
@@ -42,6 +42,7 @@ export function TranslationOptionsContainer({
 					values.businessType === 'Integer' ||
 					values.businessType === 'LongInteger' ||
 					values.businessType === 'MultiselectPicklist' ||
+					values.businessType === 'Picklist' ||
 					values.businessType === 'PrecisionDecimal'))) &&
 		!values.system;
 
